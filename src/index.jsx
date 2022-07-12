a571abbd2000e70ab2024c6aa71c306661aa1a7c
Carter Sellgren <cartersellgren@Pro.local>
2022-07-11 22:37:17 -0700

    changed spacing

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import MainPage from './mainpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <MainPage />
-  </React.StrictMode>
+    <React.StrictMode>
+        <MainPage />
+    </React.StrictMode>
 );
